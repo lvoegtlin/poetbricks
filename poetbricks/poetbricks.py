ec531a26d7d942e3ffdb6d84da81b00749e04b12
Lars Voegtlin <lvoegtlin@gmail.com>
2024-11-01 17:59:21 +0100

    :construction: :recycle: refactoring ok

--- poetbricks/poetbricks.py
+++ poetbricks/poetbricks.py
@@ -1,20 +1,38 @@
 from pathlib import Path
 
-from poetbricks.requirements import PoetryRequirement, PythonRequirement
+from poetbricks.requirements import (
+    DBXPIPRequirement,
+    PoetryRequirement,
+    PythonRequirement,
+)
 
 
 class Poetbricks:
-    def __init__(self):
-        pass
+    def __init__(self, dbx_req: DBXPIPRequirement, poetry_req: PoetryRequirement):
+        self.dbx_req: DBXPIPRequirement = dbx_req
+        self.poetry_req: PoetryRequirement = poetry_req
 
     @classmethod
-    def from_project_file(cls, project_file_path: Path) -> PythonRequirement:
-        return PoetryRequirement()
-        pass
+    def from_project_file(cls, project_file_path: Path, version: float) -> "Poetbricks":
+        dbx: DBXPIPRequirement = DBXPIPRequirement(version=version)
+        poetry: PoetryRequirement = PoetryRequirement(file_path=project_file_path)
 
-    def write_pip_requirement_file(self, output_path: Path) -> None:
-        pass
+        dbx.load_requirement_file()
+        poetry.load_requirement_file()
+
+        return Poetbricks(dbx_req=dbx, poetry_req=poetry)
+
+    def write_pip_requirement_file(self, output_path: Path, override: bool) -> None:
+        complement = PythonRequirement.complement_requirements(
+            source=self.poetry_req, check=self.dbx_req
+        )
+        print(complement)
+        # PythonRequirement.save_complement_requirement_file(
+        #    requirements=complement, output_path=output_path, override=override
+        # )
 
 
 if __name__ == "__main__":
-    p = Poetbricks()
+    p = Poetbricks.from_project_file(Path("."), version=15.3)
+
+    p.write_pip_requirement_file(Path("."), False)
--- poetbricks/requirements.py
+++ poetbricks/requirements.py
@@ -1,10 +1,17 @@
+import json
+import logging
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Any, Dict
 
+import requests
 import tomllib
 
 DBX_REQUIREMENT_URL = "https://docs.databricks.com/en/_extras/documents/"
+POETBRICKS_SETTINGS_ROOT_PATH = Path("~/.poetbricks").expanduser()
+POETBRICKS_DBX_REQUIREMENT_PATH = POETBRICKS_SETTINGS_ROOT_PATH / "dbx_req"
+
+logger = logging.getLogger("requirements")
 
 
 class PythonRequirement(ABC):
@@ -20,25 +27,92 @@ class PythonRequirement(ABC):
     def requirements(self) -> Dict:
         return self._requirements
 
+    @requirements.setter
+    def requirements(self, value: Dict[str, str]):
+        self._requirements = value
+
+    @property
+    def source_path(self) -> Path:
+        """doc"""
+        return self._source_path
+
+    @source_path.setter
+    def source_path(self, value: Path):
+        self._source_path = value
+
     @classmethod
     def complement_requirements(
         cls,
-        first_requirement: "PythonRequirement",
-        second_requirement: "PythonRequirement",
+        source: "PythonRequirement",
+        check: "PythonRequirement",
     ) -> Dict[str, str]:
         return {
             k: v
-            for k, v in first_requirement._requirements.items()
-            if k not in second_requirement._requirements.keys()
+            for k, v in source.requirements.items()
+            if k not in check.requirements.keys()
         }
+
+    @classmethod
+    def save_complement_requirement_file(
+        cls, requirements: Dict[str, str], output_path: Path, override: bool
+    ) -> None:
+        requirement_file_path = output_path.parent / "requirements.txt"
+        if requirement_file_path.exists() and override:
+            raise ValueError(
+                f"Requirement file exists in {output_path.parent} and override is not allowed!"
+            )
+        file_content_list = [f"{k}=={v}" for k, v in requirements.items()]
+        with requirement_file_path.open("w") as f:
+            f.write("\n".join(file_content_list))
 
 
 class DBXPIPRequirement(PythonRequirement):
     def __init__(self, version: float):
         self.version = version
 
+    def load_requirement_file(self) -> None:
+        # check if file exists
+        if self._check_dbx_file_exists():
+            logger.info("DBX requirement file existing. Loading...")
+            self.requirements = self._get_requirement_file()
+            return
+
+        self._get_requirement_dict_from_server()
+        self._save_req_dict()
+
+    def _get_requirement_file(self) -> Dict:
+        file_path = POETBRICKS_DBX_REQUIREMENT_PATH / f"{self.version}.json"
+        with file_path.open("r") as f:
+            return json.load(f)
+
     def _create_requirement_url(self):
         return DBX_REQUIREMENT_URL + f"requirements-{self.version}.txt"
+
+    def _check_dbx_file_exists(self) -> bool:
+        return (POETBRICKS_DBX_REQUIREMENT_PATH / f"{self.version}.json").exists()
+
+    def _save_req_dict(self) -> None:
+        logger.info("Saving DBX requirements file...")
+        with (POETBRICKS_DBX_REQUIREMENT_PATH / f"{self.version}.json").open("w") as f:
+            json.dump(self.requirements, f)
+
+    def _get_requirement_dict_from_server(self) -> None:
+        logger.info("Downloading missing requirements file...")
+        req_url = f"https://docs.databricks.com/en/_extras/documents/requirements-{self.version}.txt"
+
+        req_file_request = requests.get(req_url, allow_redirects=True)
+        req_file_content = req_file_request.content.decode("UTF-8")
+        req_dict = {
+            line.split("==")[0]: line.split("==")[1]
+            for line in req_file_content.split("\n")
+            if line != ""
+        }
+        self.requirements = req_dict
+
+
+class PoetryRequirement(PythonRequirement):
+    def __init__(self, file_path: Path):
+        self._source_path: Path = file_path
 
     def load_requirement_file(self) -> None:
         toml_path: Path = self._source_path / "pyproject.toml"
@@ -52,17 +126,9 @@ class DBXPIPRequirement(PythonRequirement):
 
             dep_dict: Dict[str, Any] = toml["tool"]["poetry"]["dependencies"]
             del dep_dict["python"]
-            self._requirements = dep_dict
+            self.requirements = dep_dict
             self._clean_version_strings()
 
     def _clean_version_strings(self):
-        for k in self._requirements.keys():
-            self._requirements[k] = self._requirements[k].replace("^", "")
-
-
-class PoetryRequirement(PythonRequirement):
-    def __init__(self):
-        pass
-
-    def load_requirement_file(self) -> None:
-        pass
+        for k in self.requirements.keys():
+            self.requirements[k] = self._requirements[k].replace("^", "")
